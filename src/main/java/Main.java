e9b0825a8aea57d894e8df55a4f39807ee5b984f
Barmin Andrey <barminav@gmail.com>
2021-09-03 04:13:09 +0300

    Initial project version

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import com.github.javaparser.utils.Pair;
+import org.apache.tools.ant.util.StringUtils;
+import org.codehaus.groovy.util.StringUtil;
+
+import java.io.ByteArrayOutputStream;
+import java.util.*;
+
+public class Main {
+
+    static Map<String, List<String>> valueData1 = new HashMap<>();
+    static Map<String, List<Integer>> valueData2 = new HashMap<>();
+
+    public static void main(String[] args) {
+        System.out.println("jenkins2");
+    }
+}
